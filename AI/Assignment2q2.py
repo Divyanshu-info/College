7ed75595f085ab471c08e44462c3fbe51d816160
Divyanshu <56086382+Divyanshu-info@users.noreply.github.com>
2021-02-15 13:56:11 +0530

    15 02 21

---
+++ AI/Assignment2q2.py
@@ -0,0 +1,16 @@
+def palindrome(s):
+    if len(s) == 1:
+        return True
+    if s[0] == s[-1]:
+        return palindrome(s[1:-1])
+    else:
+        return False
+
+
+a = input("Enter string: ")
+if len(a) < 1:
+    print("Invalid String")
+elif(palindrome(a) == True):
+    print("String is a Palindrome")
+else:
+    print("String is not a Palindrome")
--- AI/Assignment2q3.py
+++ AI/Assignment2q3.py
@@ -1,11 +1,18 @@
 def add(x, y):
     return x + y
+
+
 def subtract(x, y):
     return x - y
+
+
 def multiply(x, y):
     return x * y
+
+
 def divide(x, y):
     return x / y
+
 
 while True:
     num1 = float(input("Enter first number:\t"))
