4900943ef4cb1f5633c7e46dd59014c3f1c1a60e
Divyanshu <divyanshu.info@gmail.com>
2021-10-10 09:37:13 +0000

    10 oct 21

---
+++ dse-meshing/run.sh
@@ -0,0 +1,4 @@
+python3 logmap_estimation/eval_network.py
+python3 logmap_alignment/align_patches.py
+python3 logmap_alignment/eval_align_meshes.py
+python3 triangle_selection/select.py
---
+++ dse-meshing/triangle_selection/postprocess/build/CMakeFiles/postprocess.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/postprocess.dir/postprocess.cpp.o"
+  "CMakeFiles/postprocess.dir/postprocess.cpp.o.d"
+  "postprocess"
+  "postprocess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/postprocess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
