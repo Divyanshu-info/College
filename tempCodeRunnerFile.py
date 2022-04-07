b80705de4d2b8f157ccd283bc1fc2a0656c854f7
Divyanshu <56086382+Divyanshu-info@users.noreply.github.com>
2022-04-07 23:48:49 +0530

    7 apr 22

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,5 @@
-import numpy as np
+from numpy.random import rand, randn
+N = 20
 
-b_matrix = [[2, 4, 4, 2, 2, 4, 4, 2],
-            [1, 1, 2, 2, 1, 1, 2, 2],
-            [1, 1, 1, 1, 2, 2, 2, 2],
-            [1, 1, 1, 1, 1, 1, 1, 1]]
-
-o_cubeVertices = ((2, 1, 1), (4, 1, 1), (4, 2, 1), (2, 2, 1),
-                  (2, 1, 2), (4, 1, 2), (4, 2, 2), (2, 2, 2))
-
-print(b_matrix)
-o_cubeVertices = np.transpose(o_cubeVertices)
-b_matrix = np.vstack([o_cubeVertices, [1, 1, 1, 1, 1, 1, 1, 1]])
-print(b_matrix)
+x = 2 * (rand(N) >= 0.5) - 1
+print(x)
